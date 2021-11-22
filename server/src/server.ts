8ed504ae869f349409e66d30aeb3f040ed983106
Matt Masson <mmasson@microsoft.com>
2021-11-22 16:03:32 -0500

    testing

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,9 +18,13 @@ interface ServerSettings {
     maintainWorkspaceCache: boolean;
 }
 
+const connectionOptions: LS.ConnectionOptions = {
+    cancellationStrategy: LS.CancellationStrategy.Message,
+};
+
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
-const connection: LS.Connection = LS.createConnection(LS.ProposedFeatures.all);
+const connection: LS.Connection = LS.createConnection(LS.ProposedFeatures.all, connectionOptions);
 const documents: LS.TextDocuments<TextDocument> = new LS.TextDocuments(TextDocument);
 
 let serverSettings: ServerSettings;
@@ -73,9 +77,11 @@ documents.onDidChangeContent(event => {
 });
 
 async function validateDocument(document: TextDocument): Promise<void> {
+    const validationToken: PQP.ICancellationToken = createValidationCancellationToken(document.uri, document.version);
+
     const result: PQLS.ValidationResult = PQLS.validate(
         document,
-        createValidationSettings(getLocalizedStandardLibrary()),
+        createValidationSettings(getLocalizedStandardLibrary(), validationToken),
     );
 
     connection.sendDiagnostics({
@@ -113,11 +119,11 @@ connection.onDocumentFormatting((documentfomattingParams: LS.DocumentFormattingP
 connection.onCompletion(
     async (
         textDocumentPosition: LS.TextDocumentPositionParams,
-        _token: LS.CancellationToken,
+        token: LS.CancellationToken,
     ): Promise<LS.CompletionItem[]> => {
         const document: TextDocument | undefined = documents.get(textDocumentPosition.textDocument.uri);
         if (document) {
-            const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position);
+            const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position, token);
 
             return analysis.getAutocompleteItems().catch(err => {
                 connection.console.error(`onCompletion error ${JSON.stringify(err, undefined, 4)}`);
@@ -132,13 +138,13 @@ connection.onCompletion(
 connection.onDocumentSymbol(
     async (
         documentSymbolParams: LS.DocumentSymbolParams,
-        _token: LS.CancellationToken,
+        token: LS.CancellationToken,
     ): Promise<LS.DocumentSymbol[] | undefined> => {
         const document: TextDocument | undefined = documents.get(documentSymbolParams.textDocument.uri);
         if (document) {
             return PQLS.getDocumentSymbols(
                 document,
-                PQP.DefaultSettings,
+                createParserSettingsWithCancellationToken(token),
                 serverSettings.maintainWorkspaceCache ?? false,
             );
         }
@@ -148,7 +154,10 @@ connection.onDocumentSymbol(
 );
 
 connection.onHover(
-    async (textDocumentPosition: LS.TextDocumentPositionParams, _token: LS.CancellationToken): Promise<LS.Hover> => {
+    async (
+        textDocumentPosition: LS.TextDocumentPositionParams,
+        _token: LS.CancellationToken,
+    ): Promise<LS.Hover | LS.ResponseError<void>> => {
         const emptyHover: LS.Hover = {
             range: undefined,
             contents: [],
@@ -159,19 +168,29 @@ connection.onHover(
             return emptyHover;
         }
 
-        const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position);
+        const token: PQP.ICancellationToken = createValidationCancellationToken(document.uri, document.version);
+
+        const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position, token);
+
+        // if (token.isCancellationRequested) {
+        //     connection.console.info(`onHover action was cancelled`);
+        //     return new LS.ResponseError(LS.LSPErrorCodes.RequestCancelled, "Request got cancelled");
+        // }
 
         return analysis.getHover().catch(err => {
             connection.console.error(`onHover error ${JSON.stringify(err, undefined, 4)}`);
             return emptyHover;
         });
+
+        // // TODO: is it safer to return the empty value rather than an error response?
+        // return handleServerResponse("onHover", analysis.getHover, emptyHover, token);
     },
 );
 
 connection.onSignatureHelp(
     async (
         textDocumentPosition: LS.TextDocumentPositionParams,
-        _token: LS.CancellationToken,
+        token: LS.CancellationToken,
     ): Promise<LS.SignatureHelp> => {
         const emptySignatureHelp: LS.SignatureHelp = {
             signatures: [],
@@ -182,7 +201,7 @@ connection.onSignatureHelp(
 
         const document: TextDocument | undefined = documents.get(textDocumentPosition.textDocument.uri);
         if (document) {
-            const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position);
+            const analysis: PQLS.Analysis = createAnalysis(document, textDocumentPosition.position, token);
 
             return analysis.getSignatureHelp().catch(err => {
                 connection.console.error(`onSignatureHelp error ${JSON.stringify(err, undefined, 4)}`);
@@ -201,15 +220,26 @@ documents.listen(connection);
 // Listen on the connection
 connection.listen();
 
-function createAnalysis(document: TextDocument, position: PQLS.Position): PQLS.Analysis {
+function createAnalysis(
+    document: TextDocument,
+    position: PQLS.Position,
+    token: LS.CancellationToken | PQP.ICancellationToken,
+): PQLS.Analysis {
     const localizedStandardLibrary: PQLS.Library.ILibrary = getLocalizedStandardLibrary();
 
-    return PQLS.AnalysisUtils.createAnalysis(document, createAnalysisSettings(localizedStandardLibrary), position);
+    return PQLS.AnalysisUtils.createAnalysis(
+        document,
+        createAnalysisSettings(localizedStandardLibrary, token),
+        position,
+    );
 }
 
-function createAnalysisSettings(library: PQLS.Library.ILibrary): PQLS.AnalysisSettings {
+function createAnalysisSettings(
+    library: PQLS.Library.ILibrary,
+    token: LS.CancellationToken | PQP.ICancellationToken,
+): PQLS.AnalysisSettings {
     return {
-        createInspectionSettingsFn: () => createInspectionSettings(library),
+        createInspectionSettingsFn: () => createInspectionSettings(library, token),
         library,
         maintainWorkspaceCache: serverSettings.maintainWorkspaceCache,
     };
@@ -219,20 +249,74 @@ function getLocalizedStandardLibrary(): PQLS.Library.ILibrary {
     return StandardLibraryUtils.getOrCreateStandardLibrary(serverSettings.locale);
 }
 
-function createInspectionSettings(library: PQLS.Library.ILibrary): PQLS.InspectionSettings {
+function createParserSettingsWithCancellationToken(token: LS.CancellationToken | PQP.ICancellationToken): PQP.Settings {
+    return {
+        ...PQP.DefaultSettings,
+        maybeCancellationToken: "isCancellationRequested" in token ? wrapCancellationToken(token) : token,
+    };
+}
+
+function createInspectionSettings(
+    library: PQLS.Library.ILibrary,
+    token: LS.CancellationToken | PQP.ICancellationToken,
+): PQLS.InspectionSettings {
     return PQLS.InspectionUtils.createInspectionSettings(
         {
-            ...PQP.DefaultSettings,
+            ...createParserSettingsWithCancellationToken(token),
             locale: serverSettings.locale,
         },
         library.externalTypeResolver,
     );
 }
 
-function createValidationSettings(library: PQLS.Library.ILibrary): PQLS.ValidationSettings {
+function createValidationSettings(
+    library: PQLS.Library.ILibrary,
+    token: LS.CancellationToken | PQP.ICancellationToken,
+): PQLS.ValidationSettings {
     return PQLS.ValidationSettingsUtils.createValidationSettings(
-        createInspectionSettings(library),
+        createInspectionSettings(library, token),
         LanguageId,
         serverSettings.checkForDuplicateIdentifiers,
     );
 }
+
+function wrapCancellationToken(token: LS.CancellationToken): PQP.ICancellationToken {
+    return {
+        isCancelled: () => token.isCancellationRequested,
+        cancel: () => {
+            /**/
+        },
+        throwIfCancelled: () => false,
+    };
+}
+
+function createValidationCancellationToken(documentUrl: string, documentVersion: number): PQP.ICancellationToken {
+    return {
+        isCancelled: () => {
+            connection.console.error("isCancelled");
+            return documents.get(documentUrl)?.version !== documentVersion;
+        },
+        cancel: () => {
+            /**/
+        },
+        throwIfCancelled: () => false,
+    };
+}
+
+// async function handleServerResponse<T>(
+//     id: string,
+//     action: () => Promise<T>,
+//     defaultValue: T,
+//     token: LS.CancellationToken,
+// ): Promise<T | LS.ResponseError<void>> {
+//     if (token.isCancellationRequested) {
+//         connection.console.info(`${id} action was cancelled`);
+//         return new LS.ResponseError(LS.LSPErrorCodes.RequestCancelled, "Request got cancelled");
+//     }
+
+//     return action().catch(err => {
+//         connection.console.error(`${id} error ${JSON.stringify(err, undefined, 4)}`);
+//         connection.console.error(`cancel token value ${token.isCancellationRequested}`);
+//         return defaultValue;
+//     });
+// }
